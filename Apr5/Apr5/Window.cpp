ad8ca7a4aa700315370d09f7aea25e3a6fe09703
Game110AM <Game110AM@605-110-STU03>
2017-04-07 07:58:47 -0600

    stuff

--- Apr5/Apr5/Window.cpp
+++ Apr5/Apr5/Window.cpp
@@ -7,16 +7,10 @@ int main() {
 	al_init_image_addon();
 	ALLEGRO_DISPLAY*display = NULL;
 	ALLEGRO_BITMAP*icon = NULL;
-	cout << "flag1" << endl;
 	display = al_create_display(640, 480);
 	icon = al_load_bitmap("doom.jpg");
-	if (icon == NULL)
-		cout << "errrr." << endl;
-	cout << "flag2" << endl;
 	al_set_window_title(display, "I have no idea what to call this.");
-	cout << "flag3" << endl;
 	al_set_display_icon(display, icon);
-	cout << "flag4" << endl;
 	al_rest(5.0);
 	al_destroy_display(display);
 }
